2f9c3e94b81660c740dccaa167a16855f64268f7
Maureen Daum <mdaum11@Maureen-Daums-MacBook-Pro.local>
2014-01-18 14:34:52 -0500

    Spotify_interface: add query function to event.js  where we want to pause spotify. Manifest: added permission to spotify.com and spotify script

--- spotify_interface.js
+++ spotify_interface.js
@@ -1,16 +1,29 @@
-function pauseSpotify() {
-	var trigger = document.createEvent("Event");
-	trigger.initEvent("click", true, true);
-	document.getElementById("play-pause").dispatchEvent(trigger);
-}
+var trigger = document.createEvent("MouseEvents");
+trigger.initEvent("click", true, true);
+//document.getElementById("play-pause").dispatchEvent(trigger); */
+window.frames['app-player'].document.getElementById('play-pause').dispatchEvent(trigger);
+//console.log(document.getElementById("play-pause"));
 
-function lookForSpotify() {
-	chrome.tabs.query({}, function(tabs) {
-	for (var i = 0; i < tabs.length; i++) {
-		chrome.tabs.sendMessage(tabs[i].id, {action: "xxx"})
-		if (document.getElementById('play-pause')) {
-			pauseSpotify();
-		}
-	}
-})
+
+//Add the chrome.tabs.query() to wherever we want to call the pause Spotify function.
+
+function checkTabChange(){
+    chrome.tabs.query( 
+        { "active" : true }, 
+        function(tabs){
+            chrome.tabs.query(
+            {
+                url: "https://play.spotify.com/*"
+            }, 
+            function(s_tab) {
+                console.log("running script");
+                var spotify = s_tab[0]; // assume we found it
+
+                chrome.tabs.executeScript(
+                    //code: 'console.log("fuck");'
+                    null, {file: "spotify_interface.js"}
+                ); 
+            });
+            
+        });
 };
