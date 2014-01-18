f5d1f0339c077886173e4ce73601528513996668
Mackenzie Shelley <kenzshelley@gmail.com>
2014-01-18 18:11:08 -0500

    Add fully functional timers, close tab function, other stuff...

--- eventpage.js
+++ eventpage.js
@@ -1,25 +1,15 @@
-// chrome.tabs.onActivated.addListener(function(info){
-// 	chrome.tabs.onUpdated.addListener(function())
-// }); 
 
-// chrome.tabs.onUpdated.addListener(function(tabs){
-// 	alert("super");
-// })
 var urls = ["facebook.com", "reddit.com"];
-
-//localStorage["wastedTime"]; 
-//localStorage["startTime"]; 
-//localStorage["endTime"];
 var interval = 4000;
-//localStorage["timer"] = false; 
-var extremeTimer;
+var closeTabTime = 40000;
+var closeTabTimer;
 var timing = false;
 var currentTabId;
-var extreme_enabled = false;
+var closeTabEnabled = false;
 var isEnabled = true;
 chrome.storage.sync.set({"timing": false});
 chrome.storage.sync.set({"wastedTime": 0});
-//chrome.storage.local.set({"random" : 3});
+
 
 
 function didMatchURL(url, bannedURLS){
@@ -31,79 +21,55 @@ function didMatchURL(url, bannedURLS){
 	} return false;
 };
 
-function timeElapsed(){
-	//alert("time elapsed");
+function pauseSpotify(){
 
 }
 
+function closeTab(){
+	chrome.tabs.remove(currentTabId);
+}
+
 function stopTimers(){
-	console.log("in stopTimers");
 	var endTime = new Date().getTime() / 1000;
 	console.log("et: " + endTime);
 	chrome.storage.sync.set({"endTime" : endTime}); 
 	chrome.storage.sync.get("startTime", function(st){
 		startTime = st.startTime;
 		var elapsed = endTime - startTime; 
-		console.log("elapsed: " + elapsed);
+		//console.log("elapsed: " + elapsed);
 		chrome.storage.sync.get("wastedTime", function(wt){
-			console.log("in get wasted time")
 			chrome.storage.sync.set({"wastedTime": (wt.wastedTime + elapsed)});
-			console.log("wasted time: " + wt.wastedTime);
+			//console.log("wasted: " + wt.wastedTime);
 		});
 	});
 	chrome.storage.sync.set({"timing" : false});
 	clearTimeout(timer);
-	clearTimeout(extremeTimer);
+	clearTimeout(closeTabTimer);
 };
 
-function extremeTimeElapsed(){
-	chrome.tabs.remove(currentTabId);
-}
-console.log("HI");
+
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     currentTabId = activeInfo.tabId;
 });
 
-function test(){
-	alert("TEST");
-}
-
 function checkTabChange(){
-	console.log("in check tab");
-	//alert("call checkTabChange");
 	chrome.tabs.query( {"active" : true }, function(tabs){
-			//alert("past active");
 			if (didMatchURL(tabs[0].url, urls)) {
-				console.log("on blocked site");
 				var st = new Date().getTime() / 1000;
 				chrome.storage.sync.set({"startTime" : st});
 				chrome.storage.sync.set({"timing" : true})	
-				chrome.storage.sync.get("timing", function(t){
-					console.log("timing");
-					console.log(t.timing);
-				});
 				timer = setTimeout(function(){timeElapsed()}, interval);
-				console.log("timer started");
-				if(extreme_enabled){
-					extremeTimer = setTimeout(function(){extremeTimeElapsed()}, 5000);
+				if(closeTabEnabled){
+					colseTabTimer = setTimeout(function(){closeTab()}, closeTabTime);
 				}	
 			} else {
-				var timing; 
 				chrome.storage.sync.get("timing", function(t){
-					timing = t.timing;
-					console.log(t.timing);
+				//if a timer was on, stop it because the user switched to a good tab
 					if(t.timing){
-						console.log("called stop");
-						//test();
-						stopTimers();  //need to call in funtion so that timer has value; can't; figure out why
+						stopTimers(); 
 					}
 				});
-				//console.log("timing   " + timing);
-				/*if(timing){
-					console.log("In if");
-					stopTimers();
-				}*/
 			}
 	});
 };
