2747069fbb5e69f20e48552494b9c0c13e74bab7
Matthew Grossman <matt@mrgrossman.com>
2014-01-18 03:21:23 -0500

    Modularized code, set up for eventual timer tests

--- eventpage.js
+++ eventpage.js
@@ -5,32 +5,42 @@
 // chrome.tabs.onUpdated.addListener(function(tabs){
 // 	alert("super");
 // })
-var urls = ["https://www.facebook.com/", "https://www.google.com/", "http://www.reddit.com/"];
+var urls = ["facebook.com", "reddit.com"];
 
 function didMatchURL(url, bannedURLS){
 	for (var i = 0; i < bannedURLS.length; ++i){
-		if (bannedURLS[i] == url) {
+		var didFind = url.search(bannedURLS[i]);
+		if (didFind >= 0) {
 			return true;
 		}
 	} return false;
 };
 
-chrome.tabs.onActivated.addListener(function(tabs){
+function stopTimers(){
+	
+};
+
+function checkTabChange(){
 	chrome.tabs.query( {"active" : true }, function(tabs){
 			if (didMatchURL(tabs[0].url, urls)) {
 				alert("You are at " + tabs[0].url);
+			} else {
+				stopTimers();
 			}
-		});
+	});
+};
+
+chrome.tabs.onActivated.addListener(function(tabs){
+	checkTabChange();
 });
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	if (changeInfo.status == "complete"){
-		chrome.tabs.query( {"active" : true }, function(tabs){
-			if (didMatchURL(tabs[0].url, urls)) {
-				alert("You are at " + tabs[0].url);
-			}
-		});
+		checkTabChange();
 	}
 });
+
+
 
 
 
