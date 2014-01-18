54808ce5f974e164a15620d78fae41fc8444abc0
Mackenzie Shelley <kenzshelley@gmail.com>
2014-01-18 03:37:52 -0500

    Messed around with timers

--- eventpage.js
+++ eventpage.js
@@ -7,6 +7,8 @@
 // })
 var urls = ["https://www.facebook.com/", "https://www.google.com/", "http://www.reddit.com/"];
 
+var wasted_time; 
+
 function didMatchURL(url, bannedURLS){
 	for (var i = 0; i < bannedURLS.length; ++i){
 		if (bannedURLS[i] == url) {
@@ -15,10 +17,19 @@ function didMatchURL(url, bannedURLS){
 	} return false;
 };
 
+function tenElapsed(){
+	alert("10 Seconds");
+
+}
+
 chrome.tabs.onActivated.addListener(function(tabs){
 	chrome.tabs.query( {"active" : true }, function(tabs){
 			if (didMatchURL(tabs[0].url, urls)) {
-				alert("You are at " + tabs[0].url);
+				//alert("You are at " + tabs[0].url);
+				var start_time = new Date().getTime() / 1000; 
+				window.setInterval(function(){tenElapsed()}, 1000);
+				
+
 			}
 		});
 });
@@ -26,7 +37,8 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	if (changeInfo.status == "complete"){
 		chrome.tabs.query( {"active" : true }, function(tabs){
 			if (didMatchURL(tabs[0].url, urls)) {
-				alert("You are at " + tabs[0].url);
+				//alert("You are at " + tabs[0].url);
+				window.setInterval(function(){alert("10 Seconds")}, 1000)
 			}
 		});
 	}
