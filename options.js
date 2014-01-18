7bd50d45476e1cf5b45c694b7392ba072e6bf7f2
Matthew Grossman <matt@mrgrossman.com>
2014-01-18 17:22:33 -0500

    Finished options page

---
+++ options.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#save").click(function(){
+      var text = $("#input").val();
+      var sites = text.split(" ");
+      chrome.storage.sync.set({'sites' : sites});
+
+    });
+});
+
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+  chrome.storage.sync.get("sites", function(retVal){
+    var strings = retVal.sites;
+    if (!retVal) {
+      return;
+    }
+    var input = $("#input");
+    for (var i = 0; i < strings.length; i++) {
+      var temp = strings[i];
+      input.append(temp + "\n");
+    }
+  });
+  
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
