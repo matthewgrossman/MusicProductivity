371e768db0d8b1b90b1da0b5936add7a99c13eeb
Maureen Daum <mdaum11@Maureen-Daums-MacBook-Pro.local>
2014-01-18 05:30:32 -0500

    back to good version

--- eventpage.js
+++ eventpage.js
@@ -5,29 +5,79 @@
 // chrome.tabs.onUpdated.addListener(function(tabs){
 //         alert("super");
 // })
-var urls = ["https://www.facebook.com/", "https://www.google.com/", "http://www.reddit.com/"];
+var urls = ["facebook.com", "reddit.com"];
+
+var wasted_time=0; 
+var start_time; 
+var end_time;
+var interval = 4000;
+var timer;
+var extremeTimer;
+var timing = false;
+var currentTabId;
+var extreme_enabled = false;
 
 function didMatchURL(url, bannedURLS){
         for (var i = 0; i < bannedURLS.length; ++i){
-                if (bannedURLS[i] == url) {
+                var didFind = url.search(bannedURLS[i]);
+                if (didFind >= 0) {
                         return true;
                 }
         } return false;
 };
 
-chrome.tabs.onActivated.addListener(function(tabs){
-        chrome.tabs.query( {"active" : true }, function(tabs){
-                        if (didMatchURL(tabs[0].url, urls)) {
-                                alert("You are at " + tabs[0].url);
-                        }
-                });
+function timeElapsed(){
+        //alert("time elapsed");
+
+}
+
+function stopTimers(){
+        end_time = new Date().getTime() / 1000; 
+        var elapsed = end_time - start_time;
+        wasted_time += elapsed; 
+        alert(elapsed);
+        alert(wasted_time);
+        timing = false;
+        clearTimeout(timer);
+        clearTimeout(extremeTimer);
+};
+
+function extremeTimeElapsed(){
+        chrome.tabs.remove(currentTabId);
+}
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    currentTabId = activeInfo.tabId;
 });
+
+function checkTabChange(){
+        //alert("call checkTabChange");
+        chrome.tabs.query( {"active" : true }, function(tabs){
+                        //alert("past active");
+                        if (didMatchURL(tabs[0].url, urls)) {
+                                start_time = new Date().getTime() / 1000;
+                                timing = true; 
+                                timer = setTimeout(function(){timeElapsed()}, interval);
+                                if(extreme_enabled){
+                                        extremeTimer = setTimeout(function(){extremeTimeElapsed()}, 5000);
+                                }        
+                        } else {
+                                //alert("else");
+                                //alert(timing);
+                                if(timing){
+                                        //alert("is timing");
+                                        stopTimers();
+                                }
+                        }
+        });
+};
+
+chrome.tabs.onActivated.addListener(function(tabs){
+        checkTabChange();
+});
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
         if (changeInfo.status == "complete"){
-                chrome.tabs.query( {"active" : true }, function(tabs){
-                        if (didMatchURL(tabs[0].url, urls)) {
-                                alert("You are at " + tabs[0].url);
-                        }
-                });
+                checkTabChange();
         }
 });
