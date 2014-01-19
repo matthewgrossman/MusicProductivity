f7a45072126a99d95aa816766f626ed142d7acca
Matthew Grossman <matt@mrgrossman.com>
2014-01-18 22:24:31 -0500

    Checked against null bannedURLs and got rid of logs

--- eventpage.js
+++ eventpage.js
@@ -12,17 +12,13 @@ chrome.storage.sync.set({"wastedTime": 0});
 function didMatchURL(url, bannedURLS){
         for (var i = 0; i < bannedURLS.length; ++i){
                 var didFind = url.search(bannedURLS[i]);
-                console.log("URL "+url);
-                console.log("bannedURLS "+ bannedURLS);
-                console.log("didFind: " + didFind);
-                if (didFind >= 0) {
+                if (didFind >= 0 && bannedURLS[i] != "") {
                         return true;
                 }
         } return false;
 };
 
 function pauseSpotify(){
-	console.log("pause");
 	chrome.tabs.query( 
         { "active" : true }, 
         function(tabs){
@@ -34,10 +30,8 @@ function pauseSpotify(){
             	if (!s_tab) {
             		return;
             	}
-                console.log("running script");
                 var spotify = s_tab[0]; // assume we found it
                 chrome.tabs.executeScript(
-                    //code: 'console.log("fuck");'
                     spotify.id, {file: "spotify_interface.js"}
                 ); 
             });
@@ -46,26 +40,21 @@ function pauseSpotify(){
 }
 
 function playSound(){
-	console.log("in play sound");
 	chrome.extension.sendMessage({action: "play"});
 }
 
 function closeTab(){
-	console.log("in closeTab");
 	chrome.tabs.remove(currentTabId);
 }
 
 function stopTimers(){
-	console.log("stop");
 	var endTime = new Date().getTime() / 1000;
 	chrome.storage.sync.set({"endTime" : endTime}); 
 	chrome.storage.sync.get("startTime", function(st){
 		startTime = st.startTime;
 		var elapsed = endTime - startTime; 
-		console.log("elapsed: " + elapsed);
 		chrome.storage.sync.get("wastedTime", function(wt){
 			chrome.storage.sync.set({"wastedTime": (wt.wastedTime + elapsed)});
-			console.log("wasted: " + wt.wastedTime);
 		});
 	});
 	chrome.storage.sync.set({"timing" : false});
@@ -132,13 +121,11 @@ function checkTabChange(){
 //if productivity mode is turned on, pay attention for bad activities
 if(isEnabled){
 	chrome.tabs.onActivated.addListener(function(tabs){
-		console.log("Checking tabChange onActivated");
 		checkTabChange();
 	});	
 
 	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 		if (changeInfo.status == "complete"){
-			console.log("Checking tabChange onUpdated");
 			checkTabChange();
 		}	
 	});
